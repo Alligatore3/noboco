eefda3225f30a13ad4eefad3ef3d65a7a7e3246d
Mattia Zanella <mattia_zanella@icloud.com>
2025-12-19 19:05:39 +0100

    Create DottedGradientSection component

--- app/components/JoinForms.vue
+++ app/components/JoinForms.vue
@@ -1,19 +1,10 @@
 <template>
-  <section class="relative px-4 py-8 overflow-hidden">
-    <!-- Ref: https://bg.ibelick.com/ -->
+  <DottedGradientSection>
     <div
-      class="absolute top-0 z-[-2] h-screen w-screen bg-white bg-[radial-gradient(100%_50%_at_50%_0%,rgba(0,163,255,0.13)_0,rgba(0,163,255,0)_50%,rgba(0,163,255,0)_100%)]"
-    />
-    <div
-      class="absolute inset-0 z-[-1] h-full w-full bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px]"
-    />
-    <Container>
-      <div
-        class="z-10 flex items-center flex-col lg:flex-row w-full justify-evenly gap-8"
-      >
-        <SMEWaitListForm />
-        <NobocoPartnersForm />
-      </div>
-    </Container>
-  </section>
+      class="z-10 flex items-center flex-col lg:flex-row w-full justify-evenly gap-8"
+    >
+      <SMEWaitListForm />
+      <NobocoPartnersForm />
+    </div>
+  </DottedGradientSection>
 </template>
