0d716ffc01a02b161f6519a0c78b6fabd5aa6824
Mattia Zanella <mattia_zanella@icloud.com>
2025-12-19 20:15:30 +0100

    Refactor app

--- app/components/WhatWeDo.vue
+++ app/components/WhatWeDo.vue
@@ -1,7 +1,21 @@
+<style scoped>
+.background-gradient {
+  background: #f5f7fa;
+  background: linear-gradient(
+    90deg,
+    rgba(245, 247, 250, 1) 0%,
+    rgba(255, 255, 255, 1) 100%
+  );
+}
+</style>
+
 <template>
   <DottedGradientSection>
     <Container>
       <div class="px-4 py-8">
+        <p class="text-blue-500 text-center md:text-left font-bold mb-2">
+          WHAT WE DO
+        </p>
         <h2
           class="text-4xl font-bold text-center md:text-left flex-1 relative line-before"
         >
@@ -9,7 +23,7 @@
           SMES
         </h2>
 
-        <p class="text-gray-500 text-center md:text-left mt-4 mb-8">
+        <p class="text-gray-500 text-center md:text-left mt-6 mb-10">
           NOBOCO bridges the gap between local ambition and global reality with
           <br />
           tailored strategies designed specifically for small and medium <br />
@@ -41,6 +55,23 @@
             icon="tabler:language"
             classes="flex-1"
           />
+        </div>
+
+        <div
+          class="py-8 px-4 md:px-16 border border-gray-200 rounded-lg mt-20 background-gradient mx-auto max-w-5xl"
+        >
+          <h3
+            class="text-2xl font-bold text-center md:text-left flex-1 relative line-before"
+          >
+            NOBOCO helps companies enter new markets through execution, local
+            partners, and an Al-powered platform — from first analysis to first
+            deal.
+          </h3>
+
+          <p class="text-center text-blue-500 mt-8 font-semibold">
+            Bridgebase is the technology layer that standardizes and scales the
+            entire process. →
+          </p>
         </div>
       </div>
     </Container>
