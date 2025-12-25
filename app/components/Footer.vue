d71c855ed41a28e100b2df815f224a6918020114
Mattia Zanella <mattia_zanella@icloud.com>
2025-12-25 23:01:37 +0100

    Add footer section

--- app/components/Footer.vue
+++ app/components/Footer.vue
@@ -10,19 +10,27 @@
           <nav>
             <ul class="flex items-center gap-4 mt-4 md:mt-0">
               <li>
-                <a href="#" class="text-sm text-gray-500">Privacy Policy</a>
+                <a href="#" class="text-sm text-gray-500">{{
+                  $t("footer.privacyPolicy")
+                }}</a>
               </li>
               <li>
-                <a href="#" class="text-sm text-gray-500">Terms of Service</a>
+                <a href="#" class="text-sm text-gray-500">{{
+                  $t("footer.termsOfService")
+                }}</a>
               </li>
-              <li><a href="#" class="text-sm text-gray-500">Contact us</a></li>
+              <li>
+                <a href="#" class="text-sm text-gray-500">{{
+                  $t("footer.contactUs")
+                }}</a>
+              </li>
             </ul>
           </nav>
         </div>
       </div>
       <Divider />
       <p class="py-4 text-sm text-center text-gray-500">
-        © 2026 NOBOCO. All rights reserved.
+        {{ $t("footer.copyright") }}
       </p>
     </Container>
   </footer>
