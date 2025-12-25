0a693f6a1feb51fec53c155e2b3fdc2d77dc0c1a
Mattia Zanella <mattia_zanella@icloud.com>
2025-12-25 22:23:33 +0100

    Add mission section

--- app/components/OurMission.vue
+++ app/components/OurMission.vue
@@ -13,20 +13,17 @@
           <h2
             class="text-4xl font-bold text-center md:text-left flex-1 relative line-before"
           >
-            Our Mission
+            {{ $t("mission.title") }}
           </h2>
 
           <div class="flex flex-col gap-6 flex-[2]">
             <p
               class="text-2xl text-blue-500 font-semibold text-center md:text-left"
             >
-              Standardize and scale global market entry.
+              {{ $t("mission.scale") }}
             </p>
             <p class="text-gray-500 text-center md:text-left">
-              We believe international expansion should not depend on luck,
-              personal networks, or ad-hoc consulting. NOBOCO makes global
-              growth predictable, operational, and scalable — for companies and
-              partners alike.
+              {{ $t("mission.description") }}
             </p>
           </div>
         </div>
@@ -35,28 +32,28 @@
 
         <div class="flex flex-col gap-2 my-12">
           <p class="text-xl font-semibold text-center md:text-left">
-            Core pillars
+            {{ $t("mission.corePillars.title") }}
           </p>
           <p class="text-gray-500 text-center md:text-left">
-            How we make it happen:
+            {{ $t("mission.corePillars.description") }}
           </p>
         </div>
 
         <div class="flex md:flex-row flex-col gap-6 justify-center">
           <ShadowBoxWithIcon
-            description="Leveraging on-the-ground knowledge to navigate new markets effectively."
+            :description="$t('mission.localExpertise.description')"
+            :title="$t('mission.localExpertise.title')"
             icon="tabler:message-language"
-            title="Local Expertise"
           />
           <ShadowBoxWithIcon
-            description="Streamlined processes to ensure efficient, compliant, and rapid market entry."
-            title="Operational Execution"
+            :description="$t('mission.operationalExecution.description')"
+            :title="$t('mission.operationalExecution.title')"
             icon="iconoir:language"
           />
           <ShadowBoxWithIcon
-            description="Data-driven insights to make global expansion predictable and measurable."
+            :description="$t('mission.intelligentCRM.description')"
+            :title="$t('mission.intelligentCRM.title')"
             icon="streamline-ultimate:analytics-board-graph-line"
-            title="Intelligent CRM"
           />
         </div>
       </div>
